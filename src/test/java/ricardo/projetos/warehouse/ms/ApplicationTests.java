3a78a380168f23439d07295bb04f693308e860a2
ricardotusto <83232446+ricardotusto@users.noreply.github.com>
2023-07-16 10:42:39 +0100

    Start

---
+++ src/test/java/ricardo/projetos/warehouse/ms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ricardo.projetos.warehouse.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
